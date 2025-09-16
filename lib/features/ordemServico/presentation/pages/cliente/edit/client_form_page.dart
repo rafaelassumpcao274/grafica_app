6d4ea72e3aa3775ed1d14529d611d6edd6bff316
Rafael <rafaelassumpcao274@gmail.com>
2025-09-16 17:24:27 -0300

    feat(): criando viewmodel no cliente

--- lib/features/ordemServico/presentation/pages/cliente/edit/client_form_page.dart
+++ lib/features/ordemServico/presentation/pages/cliente/edit/client_form_page.dart
@@ -5,154 +5,79 @@ import '../../../../domain/entities/clientes.dart';
 import '../../../providers/clientes_provider_refactored.dart';
 import '../../../widgets/components/cpf_cnpj_form_field.dart';
 import '../../../widgets/components/custom_text_input.dart';
+import 'client_form_view_model.dart';
 
-class ClientForm extends ConsumerStatefulWidget {
+class ClientForm extends ConsumerWidget {
   final String? clienteId;
 
   const ClientForm({super.key, this.clienteId});
 
   @override
-  ConsumerState<ClientForm> createState() => _ClientFormState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final clienteNotifierAsync = ref.watch(clientesNotifierProvider);
 
-class _ClientFormState extends ConsumerState<ClientForm> {
-  final _formKey = GlobalKey<FormState>();
-  final _nameController = TextEditingController();
-  final _emailController = TextEditingController();
-  final _cpfController = TextEditingController();
-  final _telefoneController = TextEditingController();
-  final _enderecoController = TextEditingController();
-  final _cidadeController = TextEditingController();
-  final _estadoController = TextEditingController();
-  final _cepController = TextEditingController();
+    return clienteNotifierAsync.when(
+      data: (clienteNotifier) {
+        final viewModel = ref.watch(clientFormViewModelProvider(clienteId));
 
-  @override
-  void dispose() {
-    _nameController.dispose();
-    _emailController.dispose();
-    _cpfController.dispose();
-    _telefoneController.dispose();
-    _enderecoController.dispose();
-    _cidadeController.dispose();
-    _estadoController.dispose();
-    _cepController.dispose();
-    super.dispose();
-  }
+        // Chama loadCliente depois que a UI for construída
+        if (clienteId != null && !viewModel.isLoading && viewModel.nameController.text.isEmpty) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            viewModel.loadCliente(clienteId!);
+          });
+        }
 
+        if (viewModel.isLoading) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
 
-  @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, _) {
-        final notifierAsync = ref.watch(clientesNotifierProvider);
-        return notifierAsync.when(
-          data: (clienteNotifier) {
-
-            if (widget.clienteId != null) {
-              final notifierAsync = ref.read(clientesNotifierProvider);
-              final clienteNotifier = notifierAsync.value;
-              if (clienteNotifier != null) {
-                clienteNotifier.getClienteById(widget.clienteId!).then( (cliente) => {
-
-                  if (cliente != null) {
-                    _nameController.text = cliente.nomeEmpresa ?? '',
-                    _emailController.text = cliente.email ?? '',
-                    _cpfController.text = cliente.getDocumentFormatted() ?? '',
-                    _telefoneController.text = cliente.telefone ?? '1',
-                }
-                });
-
-              }
-            }
-
-
-            return Scaffold(
-              appBar: AppBar(
-                title: Text(widget.clienteId != null
-                    ? 'Editar Cliente'
-                    : 'Cadastrar Cliente'),
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Form(
-                  key: _formKey,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomTextInput(
-                          controller: _nameController,
-                          hintText: 'Nome da Empresa',
-                          icon: Icons.domain,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Informe o nome da empresa';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(height: 16.0),
-                        CustomTextInput(
-                          controller: _emailController,
-                          hintText: 'E-mail',
-                          icon: Icons.alternate_email,
-                        ),
-                        const SizedBox(height: 16.0),
-                        CpfCnpjFormField(
-                          controller: _cpfController,
-                        ),
-                        const SizedBox(height: 16.0),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xD818971C),
-                              foregroundColor: Colors.white,
-                              padding: const EdgeInsets.symmetric(vertical: 18),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                            onPressed: () async {
-                              if (_formKey.currentState!.validate()) {
-                                final cliente = Clientes(
-                                  id: widget.clienteId,
-                                  nomeEmpresa: _nameController.text,
-                                  email: _emailController.text,
-                                  documento: _cpfController.text,
-                                  telefone: _telefoneController.text,
-                                  contato: _telefoneController.text,
-                                );
-                                if (widget.clienteId != null) {
-                                  await clienteNotifier.updateCliente(cliente);
-                                } else {
-                                  await clienteNotifier.addCliente(cliente);
-                                }
-                                Navigator.pop(context);
-                              }
-                            },
-                            child: Text(
-                              widget.clienteId != null
-                                  ? 'Salvar Alterações'
-                                  : 'Salvar',
-                              style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ],
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(clienteId != null ? 'Editar Cliente' : 'Cadastrar Cliente'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: GlobalKey<FormState>(),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomTextInput(
+                      controller: viewModel.nameController,
+                      hintText: 'Nome da Empresa',
+                      icon: Icons.domain,
                     ),
-                  ),
+                    const SizedBox(height: 16),
+                    CustomTextInput(
+                      controller: viewModel.emailController,
+                      hintText: 'E-mail',
+                      icon: Icons.alternate_email,
+                    ),
+                    const SizedBox(height: 16),
+                    CpfCnpjFormField(controller: viewModel.cpfController),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          await viewModel.saveCliente(clienteId);
+                          Navigator.pop(context);
+                        },
+                        child: Text(clienteId != null ? 'Salvar Alterações' : 'Salvar'),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            );
-          },
-          loading: () => const Center(child: CircularProgressIndicator()),
-          error: (err, stack) => Center(child: Text('Erro: $err')),
+            ),
+          ),
         );
       },
+      loading: () => const Scaffold(body: Center(child: CircularProgressIndicator())),
+      error: (err, stack) => Scaffold(body: Center(child: Text('Erro: $err'))),
     );
   }
 }
---
+++ lib/features/ordemServico/presentation/pages/cliente/edit/client_form_view_model.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../domain/entities/clientes.dart';
+import '../../../providers/clientes_provider_refactored.dart';
+
+final clientFormViewModelProvider = ChangeNotifierProvider.family<ClientFormViewModel, String?>((ref, clienteId) {
+  final clienteNotifier = ref.read(clientesNotifierProvider).value!;
+  final vm = ClientFormViewModel(clienteNotifier);
+  if(clienteId != null){
+    vm.loadCliente(clienteId); // já carrega aqui
+  }
+  return vm;
+});
+
+
+
+class ClientFormViewModel extends ChangeNotifier {
+  final ClientesNotifier clienteNotifier;
+
+  ClientFormViewModel(this.clienteNotifier);
+
+  // Controllers
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final cpfController = TextEditingController();
+  final telefoneController = TextEditingController();
+  final enderecoController = TextEditingController();
+  final cidadeController = TextEditingController();
+  final estadoController = TextEditingController();
+  final cepController = TextEditingController();
+
+  bool isLoading = false;
+
+  // Carrega cliente existente
+  Future<void> loadCliente(String clienteId) async {
+    isLoading = true;
+    notifyListeners();
+    clearControllers();
+
+    final cliente = await clienteNotifier.getClienteById(clienteId);
+    if (cliente != null) {
+      nameController.text = cliente.nomeEmpresa ?? '';
+      emailController.text = cliente.email ?? '';
+      cpfController.text = cliente.getDocumentFormatted() ?? '';
+      telefoneController.text = cliente.telefone ?? '';
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void clearControllers() {
+    // Limpa os controllers antes de carregar o novo cliente
+    nameController.clear();
+    emailController.clear();
+    cpfController.clear();
+    telefoneController.clear();
+    enderecoController.clear();
+    cidadeController.clear();
+    estadoController.clear();
+    cepController.clear();
+  }
+
+
+  // Salvar ou atualizar cliente
+  Future<void> saveCliente(String? clienteId) async {
+    final cliente = Clientes(
+      id: clienteId,
+      nomeEmpresa: nameController.text,
+      email: emailController.text,
+      documento: cpfController.text,
+      telefone: telefoneController.text,
+      contato: telefoneController.text,
+    );
+
+    if (clienteId != null) {
+      await clienteNotifier.updateCliente(cliente);
+    } else {
+      await clienteNotifier.addCliente(cliente);
+    }
+  }
+}
